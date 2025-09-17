67db1b6dc60b23e14a0b8ffb74dd14d55f6abc95
abhishekmaury <abhishek.einnovation@gmail.com>
2025-09-17 12:29:26 +0530

    git push

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const  environment = {
   production: false,
-   apiUrl: 'https://api.wicketwatch.com/api/v1',
+  //apiUrl: 'https://api.wicketwatch.com/api/v1',
+  apiUrl: 'http://localhost:7000/api/v1',
   userApiUrl: 'http://localhost:3001/api/v1',
   authApiUrl: 'http://localhost:3002/api/v1',
   authUrl: 'http://localhost:3002/api/v1/2fa',
