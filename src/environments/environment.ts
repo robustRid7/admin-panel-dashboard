3fbfccbbf1f4d126cb4ebdbcc674d68ac246150e
robustRid7 <rhythm.dholliya.07@gmail.com>
2025-09-30 12:00:34 +0530

    point url changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const  environment = {
   production: false,
-  apiUrl: 'https://api.wicketwatch.com/api/v1',
+  apiUrl: 'https://api.hub999.live/api/v1',
   //apiUrl: 'http://localhost:7000/api/v1',
   userApiUrl: 'http://localhost:3001/api/v1',
   authApiUrl: 'http://localhost:3002/api/v1',
